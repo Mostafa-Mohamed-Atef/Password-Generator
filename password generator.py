9968383ff3e79d83ec6defeb1123e4be12682087
Mostafa-Mohamed-Atef <mostafamohamedatef27@gmail.com>
2024-06-01 00:41:10 +0300

    still not finished

--- password generator.py
+++ password generator.py
@@ -1,10 +1,30 @@
+from numpy import random
 uppercase_alphabets = [chr(i) for i in range(65,91)]
 lowercase_alphabets = [chr(i) for i in range(97,123)]
-
-numbers = [num for num in range(10)]
+num = [str(i) for i in range(10)]
 special_characters = [
     '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', 
     '{', '}', '[', ']', '|',':', ';', '"', '<', '>', ',', 
     '.', '?', '/', '~'
 ]
 
+length = int(input('Enter password length: '))
+
+print("Answer with 'y' and 'n' by this order : \n")
+
+ifchar = input('Characters?').lower()
+password = ''
+if ifchar == 'n':
+    for i in range(length):
+        password = password + random.choice(num)
+elif ifchar == 'y':
+    prop = input("Uppercase? \n Special Characters? \n")
+    if prop[0] == 'y':
+        lowercase_alphabets.extend(uppercase_alphabets)
+    if prop[1] == 'y':
+        lowercase_alphabets.extend(special_characters)  
+    lowercase_alphabets.extend(num)
+    for i in range(length):
+        password += random.choice(lowercase_alphabets)
+
+print('Output:',password)
