f6a4deed9b076618bf217913c8bde9677a1ee566
Mostafa-Mohamed-Atef <mostafamohamedatef27@gmail.com>
2024-05-31 23:35:28 +0300

    first

---
+++ pass.py
@@ -0,0 +1,10 @@
+uppercase_alphabets = [chr(i) for i in range(65,91)]
+lowercase_alphabets = [chr(i) for i in range(97,123)]
+
+numbers = [num for num in range(10)]
+special_characters = [
+    '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', 
+    '{', '}', '[', ']', '|',':', ';', '"', '<', '>', ',', 
+    '.', '?', '/', '~'
+]
+
